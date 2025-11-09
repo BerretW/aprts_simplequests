d1294dcd53bf0260f8cb00b00397aca9abe07851
Berretw <berretw01@gmail.com>
2025-11-09 12:50:47 +0100

    aaa

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ lua54 'yes'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
 author 'SpoiledMouse'
-version '1.1'
+version '1.2'
 description 'aprts_simplequests'
 
 games {"rdr3"}
