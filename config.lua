d89b0d1f20b85810a0982c6dd38145f8eaed90ba
Berretw <berretw01@gmail.com>
2025-11-09 12:48:50 +0100

    asd

--- config.lua
+++ config.lua
@@ -1,11 +1,5 @@
 Config = {}
-Config.Debug = true
-Config.DST = 2
-Config.GreenTimeStart = 16
-Config.GreenTimeEnd = 23
-Config.ActiveTimeStart = 23
-Config.ActiveTimeEnd = 3
-
+Config.Debug = false
 
 Config.WebHook = ""
 Config.ServerName = 'WestHaven ** Loger'
