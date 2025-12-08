d871e4bc2890423ef2126864386b3b177ef7e0c7
Berretw <berretw01@gmail.com>
2025-12-08 20:17:37 +0100

    asda

--- editor/config.py
+++ editor/config.py
@@ -2,11 +2,11 @@
 
 # Zde vyplňte přihlašovací údaje k vaší MySQL/MariaDB databázi.
 DB_CONFIG = {
-    "host": "10.42.0.0",
-    "user": "westhaven",
-    "password": "Jrqhxi01whCPRISmobYYSWs",  # <-- Zadejte sem své heslo
-    "database": "westhaven_dev", # <-- Zadejte název vaší databáze
-    "port": 3317
+    "host": "cloud.appartus.cz",
+    "port": 3336,
+    "user": "user",
+    "password": "ashjkdgKLghashjkl12al",
+    "database": "VORP"
 }
 
 IMAGE_BASE_URL = "http://10.42.0.0:5000/image/"
